57b7555d224728755151f998f54c72c5ee5587fc
Ankush Jha <67179973+CR4ZED@users.noreply.github.com>
2024-08-09 00:43:35 +0530

    feat: add portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         alt="Beams background"
         className="absolute top-[20%] left-1/2 max-w-none -translate-x-1/2 md:-translate-y-1/2 -translate-y-[350%] pointer-events-none w-screen"
       />
-      <div className="absolute inset-0 bg-[url(./assets/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
+      <div className="absolute inset-0 bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
       <App />
     </div>
   </React.StrictMode>
